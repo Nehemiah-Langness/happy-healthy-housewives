1a4d3afc674910111fa3b3e909efdb4af59e8f29
Nehemiah Langness <Nehemiah.Langness@hotmail.com>
2025-08-26 14:42:30 -0500

    fixed link error

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     server: {
         port: 3500,
     },
-    base: baseUrl,
+    base: baseUrl ? baseUrl : undefined,
     css: {
         preprocessorOptions: {
            scss: {
